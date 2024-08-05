2dec9a1c584170d8677460e9e26c664d53cb5eca
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-05 22:19:23 +0530

    chore: Add HeaderBox component for displaying headers and subtext

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import HeaderBox from '../../components/HeaderBox'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	const loggedIn = {firstName: 'Adrian'};
+
+	return (
+		<div className='home'>
+			<div className='home-content'>
+				<header className='home-header'>
+					<HeaderBox
+						type='greeting'
+						title='Welcome'
+						user={loggedIn?.firstName || 'Guest'}
+						subtext='Access and manage your account and transactions efficiently.'
+					/>
+				</header>
+			</div>
+		</div>
+	)
 }
 
 export default Home
