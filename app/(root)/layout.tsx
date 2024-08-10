6ddc782f66fd7b09a05fef823843abfd7732f521
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-10 10:20:03 +0530

    chore: Update package.json with @radix-ui/react-dialog dependency and update HeaderBox.tsx to fix spacing issue

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import SideBar from "@/components/SideBar";
+import Image from 'next/image';
 
 export default function RootLayout({
 	children,
@@ -9,7 +11,18 @@ export default function RootLayout({
 	return (
 		<main className="flex h-screen w-full font-inter">
 			<SideBar user={loggedIn} />
-			{children}
+			<div className="flex size-full flex-col">
+				<div className="root-layout">
+					<Image
+						src="/icons/logo.svg"
+						alt="Menu icon"
+						width={30}
+						height={30}
+					/>
+					<MobileNav user={loggedIn} />
+				</div>
+				{children}
+			</div>
 		</main>
 	);
 }
