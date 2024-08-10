fbfc6e4865e4d657335b01d3ddcc7dfd49e05011
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-10 16:46:11 +0530

    chore: Add BankCard component for displaying bank information in RightSideBar

---
+++ components/BankCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
+    return (
+        <div>BankCard</div>
+    )
+}
+
+export default BankCard
--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import BankCard from './BankCard'
 
 const RightSideBar = ({ user, transactions, banks }: RightSidebarProps) => {
     return (
@@ -44,9 +45,26 @@ const RightSideBar = ({ user, transactions, banks }: RightSidebarProps) => {
                 {banks?.length > 0 && (
                     <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
                         <div className='relative z-10'>
-                             
+                            <BankCard
+                                key={banks[0].$id}
+                                account={banks[0]}
+                                userName={`${user.firstName} ${user.lastName}`}
+                                showBalance={false}
+                            />
                         </div>
-                        
+
+                        {
+                            banks[1] && (
+                                <div className='absolute right-0 top-0 z-0 w-[90%]'>
+                                    <BankCard
+                                        key={banks[1].$id}
+                                        account={banks[1]}
+                                        userName={`${user.firstName} ${user.lastName}`}
+                                        showBalance={false}
+                                    />
+                                </div>
+                            )
+                        }
                     </div>
                 )}
             </section>
