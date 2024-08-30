e1b9a423a4883efa5674510d01f775429dd30694
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-30 20:16:29 +0530

    refactor: Update AuthForm component to use email instead of username for form input

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -92,18 +92,34 @@ const AuthForm = ({ type }: { type: String }) => {
                                         placeholder='Password'
                                     />
 
-                                    <Button type="submit" className='form-btn' disabled={isLoading}>
-                                        {
-                                            isLoading ? (
-                                                <>
-                                                    <Loader2 size={20} className='animate-spin' />
-                                                    &nbsp; Loading...
-                                                </>
-                                            ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'
-                                        }
-                                    </Button>
+                                    <div className='flex flex-col gap-4'>
+                                        <Button type="submit" className='form-btn' disabled={isLoading}>
+                                            {
+                                                isLoading ? (
+                                                    <>
+                                                        <Loader2 size={20} className='animate-spin' />
+                                                        &nbsp; Loading...
+                                                    </>
+                                                ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'
+                                            }
+                                        </Button>
+                                    </div>
                                 </form>
                             </Form>
+
+                            <footer className="flex justify-center gap-1">
+                                <p className='text-14 font-normal text-gray-600'>
+                                    {
+                                        type === 'sign-in' ? "Don't have an account?" : 'Already have an account?'
+                                    }
+                                </p>
+
+                                <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                                    {
+                                        type === 'sign-in' ? 'Sign up' : 'Sign in'
+                                    }
+                                </Link>
+                            </footer>
                         </>
                     )
             }
