d90ed67ab579c3c7c2255058b24c56c5204a5d07
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-07 14:43:23 +0530

    refactor: Update PaymentTransfer page and add PaymentTransferForm component

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,14 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const PaymentTransfer = () => {
     return (
-        <div>PaymentTransfer</div>
+        <section className="payment-transfer">
+            <HeaderBox title='Payment Transfer' subtext='Please provide any specific details or noted related to the payment transfer.' />
+            <section className="size-full pt-5">
+
+            </section>
+        </section>
     )
 }
 
