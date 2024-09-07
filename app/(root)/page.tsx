93bf9c9a9285d3f8a3691415a2a12ec161b2f9e6
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-07 12:10:18 +0530

    feat: Add RecentTransactions component and update Home component
    This commit adds the RecentTransactions component to display recent transactions and account details. It also updates the Home component to fetch and display user accounts and transactions. The RecentTransactions component takes in the accounts data, transactions, appwriteItemId, and the current page number as props.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,10 @@ import TotalBalanceBox from '../../components/TotalBalanceBox'
 import RightSideBar from '@/components/RightSideBar';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import RecentTransactions from '@/components/RecentTransactions';
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+	const currentPage = Number(page as string) || 1;
 	const loggedIn = await getLoggedInUser();
 	const accounts = await getAccounts({
 		userId: loggedIn?.$id,
@@ -36,6 +38,13 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 						totalCurrentBalance={accounts?.totalCurrentBalance}
 					/>
 				</header>
+
+				<RecentTransactions
+					accounts={accountsData}
+					transactions={account?.transactions}
+					appwriteItemId={appwriteItemId}
+					page={currentPage}
+				/>
 			</div>
 
 			<RightSideBar
